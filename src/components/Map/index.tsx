11f69fb8277ac9eec1a5ee1b6330570088ee2ce1
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-19 09:44:36 -0300

    create Map component

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,41 @@
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+
+type Place = {
+  id: string;
+  name: string;
+  slug: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+};
+
+export type MapProps = {
+  places?: Place[];
+};
+
+export function Map({ places }: MapProps) {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={3}
+      style={{ width: '100%', height: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {places?.map(place => {
+        const { latitude, longitude } = place.location;
+
+        return (
+          <Marker
+            key={`place-${place.id}`}
+            position={[latitude, longitude]}
+            title={place.name}
+          />
+        );
+      })}
+    </MapContainer>
+  );
+}
