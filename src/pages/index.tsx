d67cfb2060b714499955d8e4d87d0b47b05e9d2f
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-19 09:46:53 -0300

    setup dynamic import to use map component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import { Main } from '@/components/Main';
+import dynamic from 'next/dynamic';
+import { InfoCircle } from '@styled-icons/bootstrap/InfoCircle';
+
+import { MapProps } from '@/components/Map';
+import { LinkWrapper } from '@/components/LinkWrapper';
+
+const Map = dynamic<MapProps>(
+  () => {
+    return import('@/components/Map').then(mod => mod.Map);
+  },
+  {
+    ssr: false,
+  },
+);
 
 export default function Home() {
-  return <Main />;
+  return (
+    <>
+      <LinkWrapper href="/about">
+        <InfoCircle />
+      </LinkWrapper>
+      <Map />
+    </>
+  );
 }
