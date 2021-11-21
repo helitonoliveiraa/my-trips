a4944e2860d89356106352ddacc459ca990d6bd2
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-20 21:16:15 -0300

    refactory to change structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,44 @@
-import dynamic from 'next/dynamic';
-import { InfoCircle } from '@styled-icons/bootstrap/InfoCircle';
+import { GetStaticProps } from 'next';
 
-import { MapProps } from '@/components/Map';
-import { LinkWrapper } from '@/components/LinkWrapper';
+import { client } from '@/graphql/client';
+import { HomeTemplate } from '@/template/Home';
+import { GET_PLACES } from '@/graphql/queries';
+import { GetPlacesQuery } from '@/graphql/generated/graphql';
 
-const Map = dynamic<MapProps>(
-  () => {
-    return import('@/components/Map').then(mod => mod.Map);
-  },
-  {
-    ssr: false,
-  },
-);
+type Place = {
+  id: string;
+  slug: string;
+  name: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  description: {
+    html: string;
+  };
+  gallery: {
+    url: string;
+    height: number;
+    width: number;
+  };
+};
 
-export default function Home() {
-  return (
-    <>
-      <LinkWrapper href="/about">
-        <InfoCircle />
-      </LinkWrapper>
-      <Map />
-    </>
-  );
+type HomeProps = {
+  places?: Place[];
+};
+
+export default function Home({ places }: HomeProps) {
+  return <HomeTemplate places={places} />;
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES);
+
+  console.log(JSON.stringify(places, null, 2));
+
+  return {
+    props: {
+      places,
+    },
+  };
+};
