f840f7b9336c5bd97ba413f2b34610a565f3fa66
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-21 11:52:34 -0300

    setup mapbox ambient variables

--- src/@types/modules.d.ts
+++ src/@types/modules.d.ts
@@ -3,5 +3,8 @@ declare namespace NodeJS {
     NODE_ENV: 'development' | 'production' | 'test';
     GRAPHQL_ENDPOINT: string;
     GRAPHQL_TOKEN: string;
+    NEXT_PUBLIC_MAPBOX_API_KEY: string;
+    NEXT_PUBLIC_MAPBOX_USERID: string;
+    NEXT_PUBLIC_MAPBOX_STYLEID: string;
   }
 }
