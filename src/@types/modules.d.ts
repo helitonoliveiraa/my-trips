78f4fbc23cc908d128c8e976d0e6d459fe23437b
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-11-22 14:56:46 -0300

    setup google analytics

--- src/@types/modules.d.ts
+++ src/@types/modules.d.ts
@@ -6,5 +6,6 @@ declare namespace NodeJS {
     NEXT_PUBLIC_MAPBOX_API_KEY: string;
     NEXT_PUBLIC_MAPBOX_USERID: string;
     NEXT_PUBLIC_MAPBOX_STYLEID: string;
+    NEXT_PUBLIC_GA_TRACKING: string;
   }
 }
---
+++ src/components/Analytics/index.tsx
@@ -0,0 +1,21 @@
+export function Analytics() {
+  return (
+    <>
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}');
+          `,
+        }}
+      />
+    </>
+  );
+}
